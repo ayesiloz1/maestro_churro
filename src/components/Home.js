1a5dc262a655bb38ef5667def29d3ed1572b4d31
ayesiloz1 <123199032+ayesiloz1@users.noreply.github.com>
2023-11-08 10:37:15 -0500

    Initial Commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Maestro Churro</h1>
+      <p>Discover the taste of authentic Spanish churros.</p>
+      {/* Add more content and styling as needed */}
+    </div>
+  );
+}
+
+export default Home;
