82834c0ce4b4ee22e370643be8ef5d8f4a190464
ayesiloz1 <123199032+ayesiloz1@users.noreply.github.com>
2023-11-20 00:08:11 -0500

    Menu and Order features added

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Footer from './components/Footer';
 import Home from './pages/Home/Home';
 import Menu from './pages/Menu';
 import OrderOnline from './pages/OrderOnline';
-import Catering from './pages/Catering';
-import AboutUs from './pages/About';
 import ContactUs from './pages/Contact';
 import Survey from './components/Survey';
 import Career from './components/Career';
@@ -22,8 +20,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/order" element={<OrderOnline />} />
-        <Route path="/catering" element={<Catering />} />
-        <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/survey" element={<Survey />} />
         <Route path="/career" element={<Career />} />
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,38 +1,23 @@
 import React from 'react';
 import BookUsSlider from './Bookus';
-import MenuSlider from './Menuslider';
 import GallerySlider from './Gallery';
 import './Home.css';
-import backgroundImage from '../../images/background2.jpg';
+import heroVideo from '../../images/hero.mp4'; // Ensure this path is correct
 
 function Home() {
-  const backgroundStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    height: '120vh',
-  };
- 
   return (
-    <div className="home" style={backgroundStyle}>
+    <div className="home">
       <main>
-        <section className="hero">
-          <h1 className="Welcome">Welcome to Maestro Churro!</h1>
-          <p className='Paragraph'>
-            Delight in the taste of our handcrafted churros, made with love and
-            passion. Experience the perfect blend of crispy and soft, sweet and 
-            savory, with our unique churro creations that promise to enchant your 
-            taste buds.
-          </p>
+        <section id="hero-section">
+          <video autoPlay muted loop>
+            <source src={heroVideo} type="video/mp4" /> {/* Use curly braces for variable */}
+            Your browser does not support the video tag.
+          </video>
         </section>
 
         <div className="grid-container">
           <div className="slider-wrapper book-us-slider">
             <BookUsSlider />
-          </div>
-          <div className="slider-wrapper menu-slider">
-            <MenuSlider />
           </div>
           <div className="slider-wrapper gallery-slider">
             <GallerySlider />
